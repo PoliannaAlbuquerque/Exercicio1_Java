10070370133b45ef27ab1fd8cca641071e50a4cd
MariaEduCavalcante <101808675+MariaEduCavalcante@users.noreply.github.com>
2022-03-25 18:40:04 -0300

    finalização do código

--- exercicio1.java
+++ exercicio1.java
@@ -5,35 +5,83 @@
  */
 package projeto2;
 
-import java.lang.reflect.Array;
 import java.util.Scanner;
 
 /**
  *
  * @author 2etimds
  */
+
 public class exercicio1 {
-     @SuppressWarnings("empty-statement")
+     {
     public static void main(String[] args) {
-      String num;
-      
-      Scanner numdigitado = new Scanner(System.in);
-      System.out.print("Escreva um número: ");
-      num = numdigitado.nextLine();
+      program();
+    }
+      public static void extenso(String a, int b){
+        String[][] unidade = {
+            {"","", "", ""},
+            
+            {"onze","um", "dez", "cento"}, 
+            
+            {"doze", "dois", "vinte", "duzentos"},
+            
+            {"treze", "três", "trinta", "trezentos"}, 
+            
+            {"quatorze", "quatro", "quarenta", "quatrocentos"},
+            
+            {"quinze", "cinco", "cinquenta", "quinhentos"}, 
+            
+            {"dezesseis", "seis", "sessenta", "seiscentos"}, 
+            
+            {"dezesete", "sete", "setenta", "setecentos"}, 
+            
+            {"dezoito", "oito", "oitenta", "oitocentos"}, 
+            
+            {"dezenove", "nove", "noventa", "novecentos"} 
+        };
+        System.out.print(unidade[ Integer.parseInt((a)) ][b]); 
+    }
+    public static void program(){
+        String num;
+        try (Scanner leitor = new Scanner(System.in)) {
+            System.out.println("Digite um número");
+            num = leitor.nextLine();
+        }
+        
+        String cen = "",
+                dez = "",
+                uni = "";
 
-      int centena = Integer.parseInt(String.valueOf(num.charAt(0)));
-      int dezena = Integer.parseInt(String.valueOf(num.charAt(1)));
-      int unidade = Integer.parseInt(String.valueOf(num.charAt(2)));
-      
-      
-      String[] centena1 = {"","cem", "duzentos", "trezentos", "quatrocentos", "quinhentos", "seiscentos", "setecentos", "oitocentos", "novecentos"};
-      String[] dezena1 = {"","", "vinte", "trinta", "quarenta", "cinquenta", "sessenta", "setenta", "oitenta", "noventa",""};
-      String[] dezena2 = {"dez","onze", "doze", "treze", "catorze", "quinze", "dezesseis", "dezesete", "dezoito", "dezenove", ""};
-      String[] unidade1 = {"","um", "dois", "três", "quatro", "cinco", "seis", "sete", "oito", "nove", ""};
-      
-      System.out.println(centena1[centena]);
-      
-   }
- }
+        uni += num.charAt(num.length() - 1);
 
+        if(num.length() > 2){ 
+            cen += num.charAt( num.length() - 3 ); 
+            if(num.charAt( num.length() - 3 ) != '0'){ 
+                extenso(cen, 3);
+                System.out.print(" e ");
 
+            }
+        }
+        
+        if(num.length() > 1){ 
+            dez += num.charAt( num.length() - 2); 
+            if(num.charAt( num.length() - 2) != '0') { 
+                if(num.charAt( num.length() - 2) == '1') 
+                    extenso(uni, 0);
+                else{
+    
+                    extenso(dez, 2);
+                    System.out.print(" e ");
+                }
+            }
+        }
+        
+        try {
+            if(num.charAt( num.length() - 2) != '1')
+            extenso(uni, 1);
+        } catch (Exception e) {
+            extenso(uni, 1);
+        }
+        
+    }
+}
